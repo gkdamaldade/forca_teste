9bd8d42b64f826cd25cc2b1647aab4fbdea22f1d
gkdamaldade <133769577+gkdamaldade@users.noreply.github.com>
2025-11-17 11:12:08 -0300

    Update main.js

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function()
   {
       const btnCadastro = document.getElementById("btnCadastro");
       if(btnCadastro){
-        btnCadastro.addEventListener("click", function()){
+        btnCadastro.addEventListener("click", function(){
           window.location.href = "cadastro.html";
         };
       }
       const btnLogin = document.getElementById("btnLogin");
       if(btnLogin){
-         btnCadastro.addEventListener("click", function()){
+         btnCadastro.addEventListener("click", function(){
           window.location.href = "login.html";
          };
       }
   });
         
                             
+
