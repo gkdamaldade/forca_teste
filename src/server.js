85322551681f2c759102ba061503defb44dd94ae
gkdamaldade <133769577+gkdamaldade@users.noreply.github.com>
2025-11-17 01:25:44 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
-const path = require('path');
 const { Server } = require('socket.io');
 
 const api = require('./routes');
