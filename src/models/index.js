1ddf71d612f52301e01e7101715f1f7ac519cf3e
Assolita <118940874+Assolita@users.noreply.github.com>
2025-11-17 16:33:40 -0300

    referenciando o arquivo de rota do sala

--- src/models/index.js
+++ src/models/index.js
@@ -4,12 +4,14 @@ const Category = require('./Category');
 const Word = require('./Word');
 const Player = require('./Player');
 const Result = require('./Result');
+const Sala = require('./Sala');
 
 const models = {};
 models.Category = Category.initModel(sequelize);
 models.Word = Word.initModel(sequelize);
 models.Player = Player.initModel(sequelize);
 models.Result = Result.initModel(sequelize);
+models.Sala = Sala.initModel(sequelize);
 
 Word.associate?.(models);
 Result.associate?.(models);
